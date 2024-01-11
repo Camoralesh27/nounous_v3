bb014de8d9cb2252df644e8fc6c71e060e2a0a59
Camoralesh27 <89227331+Camoralesh27@users.noreply.github.com>
2024-01-10 23:55:51 -0600

    realizando faq dropdown

--- js/menu.js
+++ js/menu.js
@@ -11,4 +11,21 @@ const showMenu = (toggleId, navId) =>{
     })
  }
  
- showMenu('navM-toggle','navM-menu')
+ showMenu('navM-toggle','navM-menu');
+
+
+ /*=============== FAQ ===============*/
+const showFAQ = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+          nav = document.getElementById(navId)
+ 
+    toggle.addEventListener('click', () =>{
+        // Add show-menu class to nav menu
+        nav.classList.toggle('show-faq')
+        // Add show-icon to show and hide menu icon
+        toggle.classList.toggle('show-icon2')
+    })
+ }
+ 
+ showFAQ('widget3-toggle','widget3-answer');
+
