eea4fd99ee1c287b4559a6dd60915063958f9402
Camoralesh27 <89227331+Camoralesh27@users.noreply.github.com>
2024-01-11 23:10:06 -0600

    cambiando dropdown js y html

--- js/menu.js
+++ js/menu.js
@@ -15,7 +15,7 @@ const showMenu = (toggleId, navId) =>{
 
 
  /*=============== FAQ ===============*/
-const showFAQ = (toggleId, navId) =>{
+/* const showFAQ = (toggleId, navId) =>{
     const toggle = document.getElementById(toggleId),
           nav = document.getElementById(navId)
  
@@ -27,5 +27,38 @@ const showFAQ = (toggleId, navId) =>{
     })
  }
  
- showFAQ('widget3-toggle','widget3-answer');
+ showFAQ('widget3-toggle','widget3-answer'); */
 
+ /* FAQ 2 */
+ const accordionContent = document.querySelectorAll(".accordion__content")
+
+ accordionContent.forEach((item, index) => {
+    let header = item.querySelector(".accordion__question");
+    header.addEventListener("click", () => {
+        item.classList.toggle("open");
+
+    let description = item.querySelector(".accordion__description");
+    if(item.classList.contains("open")){
+        description.style.height = `${description.scrollHeight}px`;
+        item.querySelector("i").classList.replace("fa-plus","fa-minus");
+    }else {
+        description.style.height = "0px";
+        item.querySelector("i").classList.replace("fa-minus","fa-plus");
+    }
+    removeOpen(index);
+
+    })
+})
+
+
+function removeOpen(index1){
+    accordionContent.forEach((item2, index2) => {
+        if(index1 != index2) {
+            item2.classList.remove("open");
+
+            let des = item2.querySelector(".accordion__description");
+            des.style.height ="0px"
+            item2.querySelector("i").classList.replace("fa-minus","fa-plus");
+        }
+    })
+}
